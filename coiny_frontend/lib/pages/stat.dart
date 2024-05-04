8ecac2dfed5c97824c9863bf774ce29a16f3f5d3
jjustmint <123088410+jjustmint@users.noreply.github.com>
2024-05-04 19:03:33 +0700

    update stat page

--- coiny_frontend/lib/pages/stat.dart
+++ coiny_frontend/lib/pages/stat.dart
@@ -1,6 +1,10 @@
+import 'dart:ffi';
+
+import 'package:coiny_frontend/barGraph/bar_Graph.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class stat extends StatefulWidget {
   const stat({super.key});
@@ -10,10 +14,15 @@ class stat extends StatefulWidget {
 }
 
 class _statState extends State<stat> {
+  List<double> financial = [
+    8500,
+    1050,
+    -450,
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.orange[100],
+        backgroundColor: const Color(0xFFFFE2D2),
         body: Center(
           child: Column(
             children: [
@@ -27,74 +36,109 @@ class _statState extends State<stat> {
                   ),
                 ),
               ),
-              Container(
-                height: 207,
-                width: 324,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Column(
-                  children: [
-                    SizedBox(height: 10),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          bottom: 14.0, left: 16.0, right: 16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Saved", style: TextStyle(fontSize: 20)),
-                          Text("1000฿", style: TextStyle(fontSize: 20)),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          bottom: 14.0, left: 16.0, right: 16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Usable Money", style: TextStyle(fontSize: 20)),
-                          Text("1000฿", style: TextStyle(fontSize: 20)),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          bottom: 14.0, left: 16.0, right: 16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Used", style: TextStyle(fontSize: 20)),
-                          Text("1000฿", style: TextStyle(fontSize: 20)),
-                        ],
-                      ),
-                    ),
-                    Divider(
-                      color: Colors.orange,
-                      height: 20,
-                      thickness: 4,
-                      indent: 16,
-                      endIndent: 16,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          bottom: 14.0, left: 16.0, right: 16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Total",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20)),
-                          Text("1000฿", style: TextStyle(fontSize: 20)),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+              SizedBox(
+                height: 300,
+                width: 300,
+                child: MyBarGraph(financial: financial),
+              ),
+              SizedBox(height: 80),
+              StatInfo(
+                Saved: financial[0].toInt(),
+                UsableMoney: financial[1].toInt(),
+                Used: financial[2].toInt(),
               )
             ],
           ),
         ));
+  }
+}
+
+class StatInfo extends StatelessWidget {
+  int Saved;
+  int UsableMoney;
+  int Used;
+  StatInfo({
+    required this.Saved,
+    required this.UsableMoney,
+    required this.Used,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 207,
+      width: 324,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: [
+          SizedBox(height: 10),
+          Padding(
+            padding:
+                const EdgeInsets.only(bottom: 14.0, left: 16.0, right: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Saved", style: TextStyle(fontSize: 20)),
+                Text(Saved.toString() + "฿",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+              ],
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(bottom: 14.0, left: 16.0, right: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Usable Money", style: TextStyle(fontSize: 20)),
+                Text(UsableMoney.toString() + "฿",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+              ],
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(bottom: 14.0, left: 16.0, right: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Used", style: TextStyle(fontSize: 20)),
+                Text(Used.toString() + "฿",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+              ],
+            ),
+          ),
+          Divider(
+            color: const Color(0xFFEDB59E),
+            height: 20,
+            thickness: 4,
+            indent: 16,
+            endIndent: 16,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(bottom: 14.0, left: 16.0, right: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Total",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                Text((Saved + UsableMoney).toString() + "฿",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
