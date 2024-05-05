c1d64b80b308d65cac3a456fac391a4ddaaafc14
Thawatchai Wongboonsiri <jj.wbsr@gmail.com>
2024-05-05 20:36:53 +0700

    [ADD]addMoney, addGoal, goalEdition

---
+++ coiny_frontend/lib/components/addGoal.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+class NumberInputButton extends StatelessWidget {
+  final Function(int) onSave;
+
+  NumberInputButton({required this.onSave});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AnotherPopup(); // Show AnotherPopup when NumberInputButton is clicked
+          },
+        );
+      },
+      child: Text('+'),
+    );
+  }
+}
+
+class AnotherPopup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Color(0xFFEDB59E),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 8),
+            child: Text(
+              'Enter your goal',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            height: 37,
+            child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Ex. Dream house',
+                labelStyle: TextStyle(color: Color(0xFFEDB59E)),
+                floatingLabelBehavior: FloatingLabelBehavior.never,
+                filled: true,
+                fillColor: Color(0xFFFFF3EC),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 12),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 8),
+            child: Text(
+              'Set goal',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            height: 37,
+            child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Ex. 1200000',
+                labelStyle: TextStyle(color: Color(0xFFEDB59E)),
+                floatingLabelBehavior: FloatingLabelBehavior.never,
+                filled: true,
+                fillColor: Color(0xFFFFF3EC),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 12),
+              ),
+            ),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        Center(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFFF5CCB4),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text(
+              'Save',
+              style: TextStyle(color: const Color(0xFF95491E)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Goal'),
+        ),
+        body: Center(
+          child: NumberInputButton(
+            onSave: (int number) {
+              print('Number saved: $number');
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ coiny_frontend/lib/components/addMoney.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+
+class NumberInputDialog extends StatefulWidget {
+  final Function(int) onSave;
+
+  NumberInputDialog({required this.onSave});
+
+  @override
+  _NumberInputDialogState createState() => _NumberInputDialogState();
+}
+
+class _NumberInputDialogState extends State<NumberInputDialog> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Color(0xFFEDB59E),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: EdgeInsets.zero, // No padding around the IconButton
+            child: IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+        ],
+      ),
+      content: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'How much money do you want to put in goal?',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 16), // Add some space between the texts and the TextField
+          Container(
+            height: 37, // Set the height of the input box
+            child: TextField(
+              controller: _controller,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Ex. 1000',
+                labelStyle: TextStyle(color: Color(0xFFEDB59E)),
+                floatingLabelBehavior: FloatingLabelBehavior.never,
+                filled: true,
+                fillColor: Color(0xFFFFF3EC), // Set the background color
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20), // Set the border radius
+                  borderSide: BorderSide.none, // Remove the border
+                ),
+                contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 12), // Set the padding
+              ),
+            ),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        Center(
+          child: Column(
+            children: [
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFF5CCB4), // Set the button's background color
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)), // Set the button's border radius
+                  ),
+                  onPressed: () {
+                    int number = int.tryParse(_controller.text) ?? 0;
+                    widget.onSave(number);
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    'Add to Saved',
+                    style: TextStyle(color: const Color(0xFF95491E)), // Set the text color
+                  ),
+                ),
+              ),
+              SizedBox(height: 8), // Add some space between buttons
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF95491E), // Set the button's background color
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)), // Set the button's border radius
+                  ),
+                  onPressed: () {
+                    // Add your onSave logic for the second button here
+                    int number = int.tryParse(_controller.text) ?? 0;
+                    widget.onSave(number);
+                    Navigator.of(context).pop();
+                    // This is just a placeholder
+                    print('Second button pressed');
+                  },
+                  child: Text(
+                    'Add to Usable Money',
+                    style: TextStyle(color: const Color(0xFFF5CCB4)), // Set the text color
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+class NumberInputButton extends StatelessWidget {
+  final Function(int) onSave;
+
+  NumberInputButton({required this.onSave});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return NumberInputDialog(onSave: onSave);
+          },
+        );
+      },
+      child: Text('Buffet Teenoi'),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Goal'),
+        ),
+        body: Center(
+          child: NumberInputButton(
+            onSave: (int number) {
+              print('Number saved: $number');
+              // Do something with the saved number
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
