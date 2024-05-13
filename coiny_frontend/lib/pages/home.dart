3f78e61a27bb572fed87c4e448b330d540f7d80b
jjustmint <123088410+jjustmint@users.noreply.github.com>
2024-05-13 23:23:43 +0700

    Update home.dart

--- coiny_frontend/lib/pages/home.dart
+++ coiny_frontend/lib/pages/home.dart
@@ -9,7 +9,6 @@ import '../homeComponent/addCategory.dart';
 import '../homeComponent/history.dart';
 
 class HomePage extends StatefulWidget {
-  // ignore: prefer_typing_uninitialized_variables
   final token;
   const HomePage({@required this.token, Key? key}) : super(key: key);
 
@@ -20,122 +19,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late int userId;
   @override
-  void initState() {
-    super.initState();
-    Map<String, dynamic> token = jsonDecode(widget.token);
-    userId = token['userId'];
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            color: Color(0xFFFFE2D2),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Mascot(),
-                  ShowMoney(),
-                  CategoriesList(),
-                  AddCategory(),
-                  History(),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class Mascot extends StatelessWidget {
-  const Mascot({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        ClipOval(
-          child: Image.asset(
-            'assets/rabbit.jpg',
-            width: 100,
-            height: 100,
-            fit: BoxFit.cover,
-          ),
-        ),
-        const SizedBox(width: 5),
-        // Box of text with positioned carrot image
-        Expanded(
-          child: Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 13.0),
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  height: 74,
-                  width: 225,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: const Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Howdy, I am Coiny. And I will help you manage your money!',
-                          style: TextStyle(
-                              fontSize: 14, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              // Positioned carrot image at bottom right
-              const Positioned(
-                bottom: -14.0, // Adjust position as needed
-                right: -14.0, // Adjust position as needed
-                child: Image(
-                  image: AssetImage('assets/carrot.png'),
-                  height: 60,
-                  width: 60,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ShowMoney extends StatefulWidget {
-  const ShowMoney({super.key});
-
-  @override
-  State<ShowMoney> createState() => _ShowMoneyState();
-}
-
-class _ShowMoneyState extends State<ShowMoney> {
-  final url = 'http://10.0.2.2:4000/transactions/balance';
-  int userId = 2;
-
   @override
   void initState() {
     super.initState();
     _getCategoryData();
     _getMoneyData();
     _getHistoryData();
+    Map<String, dynamic> token = jsonDecode(widget.token);
+    userId = token['userId'];
   }
 
   final urlgetcategory = 'http://10.0.2.2:4000/categories/get';
