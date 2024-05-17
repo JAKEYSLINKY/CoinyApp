ecdfe71910e11e70c6bb6709eb8db183600811a3
Navin <ITdansaikul@gmail.com>
2024-05-18 01:26:03 +0700

    [EDIT]carrot

--- coiny_frontend/lib/homeComponent/mascot.dart
+++ coiny_frontend/lib/homeComponent/mascot.dart
@@ -14,16 +14,26 @@ class _mascotState extends State<mascot> {
   late int randomNumber = random.nextInt(10);
 
   final List<String> messages = [
-    'Howdy, I am Coiny. And I will help you manage your money! Howdy, I am Coiny. And I will help you manage your money!',
-    'Hello! Coiny here, let\'s get your finances in order! Howdy, I am Coiny. And I will help you manage your money!',
-    'Greetings from Coiny! Ready to save some money? Howdy, I am Coiny. And I will help you manage your money!',
-    'Hi there! I\'m Coiny, your financial assistant. Howdy, I am Coiny. And I will help you manage your money!',
-    'Hey! Coiny at your service for all money matters. Howdy, I am Coiny. And I will help you manage your money!',
-    'Welcome! Coiny is here to help you budget wisely.Howdy, I am Coiny. And I will help you manage your money!',
-    'Good day! Coiny will assist you in tracking expenses. Howdy, I am Coiny. And I will help you manage your money!',
-    'Hi! Coiny reporting for duty to manage your funds. Howdy, I am Coiny. And I will help you manage your money!',
-    'Salutations! Coiny will help you keep your money in check. Howdy, I am Coiny. And I will help you manage your money!',
-    'Hello! Coiny is ready to support your financial goals. Howdy, I am Coiny. And I will help you manage your money!'
+    'Howdy, I am Coiny. And I will help you manage your money!',
+    'Hello! Coiny here, let\'s get your finances in order!',
+    'Greetings from Coiny! Ready to save some money?',
+    'Hi there! I\'m Coiny, your financial assistant.',
+    'Hey! Coiny at your service for all money matters.',
+    'Welcome! Coiny is here to help you budget wisely.',
+    'Good day! Coiny will assist you in tracking expenses.',
+    'Hi! Coiny reporting for duty to manage your funds.',
+    'Salutations! Coiny will help you keep your money in check.',
+    'Hello! Coiny is ready to support your financial goals.'
+    // 'Howdy, I am Coiny. And I will help you manage your money! ',
+    // 'Hello! Coiny here, let\'s get your finances in order! ',
+    // 'Greetings from Coiny! Ready to save some money? Howdy, ',
+    // 'Hi there! I\'m Coiny, your financial assistant. Howdy, ',
+    // 'Hey! Coiny at your service for all money matters. Howdy, ',
+    // 'Welcome! Coiny is here to help you budget wisely.Howdy, ',
+    // 'Good day! Coiny will assist you in tracking expenses. Howdy, ',
+    // 'Hi! Coiny reporting for duty to manage your funds. Howdy, ',
+    // 'Salutations! Coiny will help you keep your money in check. ',
+    // 'Hello! Coiny is ready to support your financial goals. Howdy, '
   ];
 
   @override
@@ -48,8 +58,6 @@ class _mascotState extends State<mascot> {
                   padding: const EdgeInsets.only(left: 13.0),
                   child: Container(
                     padding: const EdgeInsets.all(10),
-                    height: 74,
-                    width: 225,
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(15),
@@ -70,8 +78,8 @@ class _mascotState extends State<mascot> {
                 ),
               ),
               const Positioned(
-                bottom: -8.0,
-                right: 0,
+                bottom: -13.0,
+                right: -12.0,
                 child: Image(
                   image: AssetImage('assets/carrot.png'),
                   height: 60,
